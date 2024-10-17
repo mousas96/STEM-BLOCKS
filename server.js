e65f0db99ac84141499ea239b119a03925f9c2f6
mousa smady <mousa@mousas-MacBook-Pro.local>
2024-10-17 15:16:11 +0400

    make it better

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('UI'));  // Serve files from the UI directory
+// Serve static files from the UI directory
+app.use(express.static('UI'));
 
+// Send the index.html file when a request comes to the root path
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/UI/index.html'));  // Adjust the path if your entry file is located elsewhere
+    res.sendFile(path.join(__dirname, 'UI', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
